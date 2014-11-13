f49b226cda4483c2a1c9d7e27c75ae2205d1434e
dicjSwaggylord <nicolas.gamemaster@mail.com>
2014-11-13 09:41:22 -0500

    haha françois la bite!

--- ProjetSite/Account/PageAccueil.aspx.designer.vb
+++ ProjetSite/Account/PageAccueil.aspx.designer.vb
@@ -12,22 +12,4 @@ Option Explicit On
 
 
 Partial Public Class PageAccueil
-
-    '''<summary>
-    '''Contrôle abcHeader.
-    '''</summary>
-    '''<remarks>
-    '''Champ généré automatiquement.
-    '''Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
-    '''</remarks>
-    Protected WithEvents abcHeader As Global.ProjetSite._header
-
-    '''<summary>
-    '''Contrôle cbaFooter.
-    '''</summary>
-    '''<remarks>
-    '''Champ généré automatiquement.
-    '''Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
-    '''</remarks>
-    Protected WithEvents cbaFooter As Global.ProjetSite._footer
 End Class
