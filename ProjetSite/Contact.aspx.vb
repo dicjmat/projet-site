41e36c660bc087c5d42aa061c5082827ffad7978
dicjSwaggylord <nicolas.gamemaster@mail.com>
2014-11-25 17:07:32 -0500

    ZXcfgh

--- ProjetSite/Contact.aspx.vb
+++ ProjetSite/Contact.aspx.vb
@@ -1,7 +1,12 @@
 ﻿Public Class Contact
     Inherits Page
+    Dim bd As P2014_Equipe2_GestionHôtelièreEntities
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        bd = New P2014_Equipe2_GestionHôtelièreEntities
+        Dim tel = From el In bd.tblHotel Select el.noTelHotel
+        Dim telcop = From el In bd.tblHotel Select el.noTelecopieur
+        Dim telres = From el In bd.tblHotel Select el.noTelReserv
 
     End Sub
 End Class
