dfbca9f0c6ab2e2432c48a37705792329b6ddb61
dicjSwaggylord <nicolas.gamemaster@mail.com>
2014-12-01 15:39:21 -0500

    sync brah!

--- ProjetSite/listechambre.aspx.vb
+++ ProjetSite/listechambre.aspx.vb
@@ -3,11 +3,11 @@
     Private _dd As String
     Private _df As String
     Private _nhotel As Integer
-    Sub New(dated As String, datef As String, noHotel As Integer)
-        _dd = dated
-        _df = datef
-        _nhotel = noHotel
-    End Sub
+    'Sub New(dated As String, datef As String, noHotel As Integer)
+    '    _dd = dated
+    '    _df = datef
+    '    _nhotel = noHotel
+    'End Sub
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
