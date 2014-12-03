330e5db2ce3d5ee2869455f561cabb348e3ba6bd
dicjmat <marc-ant1@hotmail.com>
2014-12-03 09:05:17 -0500

    chambre description

--- ProjetSite/Chambre.aspx.vb
+++ ProjetSite/Chambre.aspx.vb
@@ -3,6 +3,30 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
+        Dim bd = New P2014_Equipe2_GestionHôtelièreEntities
+        Dim chambre = From ch In bd.tblTypeSalle
+                  Where ch.codeTypeSalle = "SUP"
+                  Select ch
+
+        Dim lit = From ch In bd.tblSalle
+                  Where ch.codeTypeSalle = "SUP"
+                  Join li In bd.tblTypeLit
+                  On li.codeTypeLit Equals ch.codeTypeLit
+                  Select li.descTypeLit
+
+
+
+
+        imgChambre.ImageUrl = "~/Images/chambre" + chambre.Single.codeTypeSalle + ".jpg"
+        lblTypeChambre.Text = chambre.Single.nomTypeSalle
+        For Each li In lit.Distinct.ToList
+            If li = lit.Distinct.ToList.Last Then
+                lblTypeLit.Text += li.ToString
+            Else
+                lblTypeLit.Text += li.ToString + " - "
+            End If
+        Next
+        lblDescTypeSalle = chambre.DescTypeSalle
     End Sub
 
 End Class
